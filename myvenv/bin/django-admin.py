3a6b56832bcaa2ac62d6fea9effc2d11bffdff0d
RyuJaesang <rjs9611@naver.com>
2020-08-04 22:09:32 +0900

    chore: delete untracked file

--- myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/ryujaesang/development/MCIP/my-community-backend/myvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
